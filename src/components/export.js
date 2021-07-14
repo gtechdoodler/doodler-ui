8f2f42bc834775481ac975ac1de24807b0c3e4d9
Mark Graham <markgr26@googlemail.com>
2021-07-14 10:42:04 +0100

    Updated comment in export.js

--- src/components/export.js
+++ src/components/export.js
@@ -8,7 +8,7 @@ export { default as Select } from './common/inputs/Select';
 export { default as SelectOption } from './common/inputs/SelectOption';
 export { default as TextField } from './common/inputs/TextField';
 
-// Inputs
+// Modal
 
 export { default as ContextMenu } from './common/modal/ContextMenu';
 export { default as Overlay } from './common/modal/Overlay';
